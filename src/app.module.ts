79dee53328e96bdc7fa415b99ab92942fb2a4db1
SongSeungYun <singsong9984@naver.com>
2025-08-06 23:13:00 +0900

    add : database setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전역 모듈로 설정
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true, // 엔티티 자동 로드
+      synchronize: true, // 개발 환경에서만 true로 설정 (DB 자동 동기화)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
