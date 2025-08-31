1792a1f6f9ab90190ed10c313817f5602c849115
SongSeungYun <singsong9984@naver.com>
2025-08-31 20:57:41 +0900

    add : enablecors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // Global Middlewares
   app.useGlobalFilters(new HttpExceptionFilter());
